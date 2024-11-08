bba83b59d2fa22dc26993e222e86c65b188689c6
STEFANO CARRARO <stefano.c@edu.pucrs.br>
2024-11-07 21:42:32 -0300

    Added money and franc, aswell as refactoring dollar and its tests

---
+++ src/main/java/verval/subject/tdd/Money.java
@@ -0,0 +1,11 @@
+package verval.subject.tdd;
+
+public class Money {
+
+    protected int amount;
+   
+    public boolean equals(Object object)  {
+      Money money = (Money) object;
+      return amount == money.amount;
+    }
+}
